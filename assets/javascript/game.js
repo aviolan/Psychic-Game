bc7a1d116f418304802d8822cf6ea35cd34d0c2e
Andrew Violan <andrew.violan@gmail.com>
2018-10-03 11:01:00 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,40 @@ var alphabet =
     ['a','b','c','d','e','f','g','h','i','j','k',
     'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-//Variables for tracking wins, losses, and ties.
-var wins = 0;
-var losses = 0;
-var guesses = 10;
+//Variables for tracking wins, losses, guesses remaining and guessed letters.
+var wins = 1;
+var losses = 1;
+var guessesRemaining = 10;
+var guessedLetters = [];
 
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     console.log(computerChoice)
 
     //When user presses a key
-document.onkeypress = function(event) {
+document.onkeydown = function(event) {
     var userGuess = event.key;
 
     if(userGuess === computerChoice){
-        wins++;
-    }else{
-        guesses--;
+        document.getElementById("wins").innerHTML = wins++;
+        guessedLetters = [];
+        document.getElementById("guessedLetters").innerHTML = guessedLetters;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(computerChoice);
+        guessesRemaining = 10;
+        document.getElementById("guessesRemaining").innerHTML = 10;
+    } else {
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
+        guessedLetters.push(userGuess);
+        document.getElementById("guessedLetters").innerHTML = guessedLetters;
     }
 
-    if(guesses === 0){
-        losses++
+    if (guessesRemaining <= 0) {
+        document.getElementById("losses").innerHTML = losses++;
+        guessesRemaining = 10;
+        guessedLetters = [];
+        document.getElementById("guessedLetters").innerHTML = guessedLetters;
+        document.getElementById("guessesRemaining").innerHTML = 10;
+        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
-    
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "Losses: " + losses;
-    document.getElementById('guesses').innerHTML = "Guesses Remaining: " + guesses;
 }
